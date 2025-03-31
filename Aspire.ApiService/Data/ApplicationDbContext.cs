227d493e20f73cf2c66b3472ace3ccf65892c908
MAGS <120091142+MAGS-GH@users.noreply.github.com>
2025-03-31 10:37:40 +0200

    Added Postgres

---
+++ Aspire.ApiService/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Aspire.ApiService.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+}
--- Aspire.AppHost/Program.cs
+++ Aspire.AppHost/Program.cs
@@ -1,8 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.Aspire_ApiService>("apiservice");
+var postgres = builder
+    .AddPostgres("postgres")
+    .WithPgAdmin(pgAdmin => pgAdmin.WithHostPort(5050))
+    .WithPgWeb()
+    .WithDataVolume(isReadOnly: false);
 
-builder.AddProject<Projects.Aspire_Web>("webfrontend")
+var postgresdb = postgres.AddDatabase("postgresdb");
+
+var apiService = builder
+    .AddProject<Projects.Aspire_ApiService>("apiservice")
+    .WithReference(postgresdb)
+    .WaitFor(postgresdb)
+    .WaitFor(postgres);
+
+builder
+    .AddProject<Projects.Aspire_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
     .WaitFor(apiService);
